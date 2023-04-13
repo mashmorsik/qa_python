a8e0a8b2be5692b64ffe69b3afc7256d7fe462c3
unknown <mary_baklanova@mail.ru>
2023-04-13 12:31:14 +0300

    sprint_2_final_project

--- tests.py
+++ tests.py
@@ -1,24 +1,102 @@
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
     def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
-
-        # добавляем две книги
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
         assert len(collector.get_books_rating()) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_add_new_book_same_title_doesnt_add(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гарри Поттер и Тайная Комната')
+        collector.add_new_book('Гарри Поттер и Тайная Комната')
+        assert len(collector.books_rating) == 1
+
+    def test_set_book_rating_less_than_11_changes_to_new(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гарри Поттер и Кубок Огня')
+        collector.set_book_rating('Гарри Поттер и Кубок Огня', 9)
+        assert collector.get_book_rating('Гарри Поттер и Кубок Огня') == 9
+
+    def test_set_book_rating_more_than_10_stays_the_same(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гарри Поттер и Узник Азкабана')
+        collector.set_book_rating('Гарри Поттер и Узник Азкабана', 11)
+        assert collector.get_book_rating('Гарри Поттер и Узник Азкабана') == 1
+
+    def test_set_book_rating_0_stays_the_same(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гарри Поттер и Орден Феникса')
+        collector.set_book_rating('Гарри Поттер и Орден Феникса', 0)
+        assert collector.get_book_rating('Гарри Поттер и Орден Феникса') == 1
+
+    def test_get_book_rating(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гарри Поттер и Философский Камень')
+        collector.set_book_rating('Гарри Поттер и Философский Камень', 9)
+        assert collector.get_book_rating('Гарри Поттер и Философский Камень') == 9
+
+    def test_get_books_with_specific_rating_two_books_return_one(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гарри Поттер и Дары Смерти')
+        collector.set_book_rating('Гарри Поттер и Дары Смерти', 8)
+        collector.add_new_book('Гарри Поттер и Проклятое Дитя')
+        collector.set_book_rating('Гарри Поттер и Проклятое Дитя', 2)
+        assert collector.get_books_with_specific_rating(8) == ['Гарри Поттер и Дары Смерти']
+
+    def test_get_books_with_specific_rating_no_books_with_wanted_rating(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гарри Поттер и Дары Смерти')
+        collector.set_book_rating('Гарри Поттер и Дары Смерти', 8)
+        collector.add_new_book('Гарри Поттер и Проклятое Дитя')
+        collector.set_book_rating('Гарри Поттер и Проклятое Дитя', 2)
+        assert collector.get_books_with_specific_rating(7) == []
+
+    def test_get_books_rating(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гарри Поттер и Философский Камень')
+        collector.set_book_rating('Гарри Поттер и Философский Камень', 9)
+        collector.add_new_book('Гарри Поттер и Тайная Комната')
+        collector.set_book_rating('Гарри Поттер и Тайная Комната', 8)
+        collector.add_new_book('Гарри Поттер и Узник Азкабана')
+        collector.set_book_rating('Гарри Поттер и Узник Азкабана', 10)
+        assert collector.get_books_rating() == {'Гарри Поттер и Философский Камень': 9,
+                                                'Гарри Поттер и Тайная Комната': 8,
+                                                'Гарри Поттер и Узник Азкабана': 10}
+
+    def test_add_book_in_favorites_add_two_books(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гарри Поттер и Узник Азкабана')
+        collector.add_new_book('Гарри Поттер и Кубок Огня')
+        collector.add_book_in_favorites('Гарри Поттер и Узник Азкабана')
+        collector.add_book_in_favorites('Гарри Поттер и Кубок Огня')
+        assert len(collector.favorites) == 2
+
+    def test_add_book_in_favorites_same_title_doesnt_add(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гарри Поттер и Узник Азкабана')
+        collector.add_new_book('Гарри Поттер и Узник Азкабана')
+        collector.add_book_in_favorites('Гарри Поттер и Узник Азкабана')
+        collector.add_book_in_favorites('Гарри Поттер и Узник Азкабана')
+        assert len(collector.favorites) == 1
+
+    def test_add_book_in_favorites_not_in_books_collector(self):
+        collector = BooksCollector()
+        collector.add_book_in_favorites('Гарри Поттер и Узник Азкабана')
+        assert collector.favorites == []
+
+    def test_delete_book_from_favorites_book_deleted(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гарри Поттер и Философский Камень')
+        collector.add_book_in_favorites('Гарри Поттер и Философский Камень')
+        collector.delete_book_from_favorites('Гарри Поттер и Философский Камень')
+        assert collector.favorites == []
+
+    def test_get_list_of_favorites_books(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гарри Поттер и Философский Камень')
+        collector.add_book_in_favorites('Гарри Поттер и Философский Камень')
+        assert collector.get_list_of_favorites_books() == ['Гарри Поттер и Философский Камень']
